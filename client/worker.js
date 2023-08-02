c1ed85e55a0cfe912aa95e314a02bc051f8c0637
wadsdaniel <danielwaduka@gmail.com>
2023-08-02 07:08:42 +0300

    Fixes

--- client/worker.js
+++ client/worker.js
@@ -3,7 +3,7 @@ self.addEventListener("push", e => {
     self.registration.showNotification(
         data.title, // title of the notification
         {
-            body: "Check out our new discounts!", //the body of the push notification
+            body: "A reminder to update your location details.", //the body of the push notification
             image: "resources/images/bell-jar-gbb4c2b869_640.png",
             icon: "resources/images/bell-jar-gbb4c2b869_640.png" // icon             
         }
